4a97ad0badf3c7f1afe85a845cd79bbd0f957369
Huzaifa3497 <116727476+Huzaifa3497@users.noreply.github.com>
2023-08-27 11:59:42 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'calcualtor_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
